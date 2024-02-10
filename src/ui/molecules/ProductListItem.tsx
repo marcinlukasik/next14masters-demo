1d864655eb899c8846f3091c48e96e9369849ca6
Marcin Lukasik <marcinlukasik.me@gmail.com>
2024-02-10 01:32:25 +0100

    feat(excersice-1): create product list

---
+++ src/ui/molecules/ProductListItem.tsx
@@ -0,0 +1,18 @@
+import { ProductListItemCoverImage } from "@/ui/atoms/ProductListItemCoverImage";
+import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription";
+import { type ProductItemType } from "@/ui/types";
+
+type ProductListItemProps = {
+	product: ProductItemType;
+};
+
+export const ProductListItem = ({ product }: ProductListItemProps) => {
+	return (
+		<li>
+			<article className="group">
+				<ProductListItemCoverImage {...product.coverImage} />
+				<ProductListItemDescription product={product} />
+			</article>
+		</li>
+	);
+};
---
+++ src/ui/organism/ProductList.tsx
@@ -0,0 +1,15 @@
+import { ProductListItem } from "@/ui/molecules/ProductListItem";
+import { type ProductItemType } from "@/ui/types";
+
+export const ProductList = ({ products }: { products: ProductItemType[] }) => {
+	return (
+		<ul
+			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
+			data-testid="products-list"
+		>
+			{products.map((product) => (
+				<ProductListItem product={product} key={product.id} />
+			))}
+		</ul>
+	);
+};
