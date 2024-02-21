cc7ee862359026c3bc3478f85dea84a65d886091
Marcin Lukasik <marcinlukasik.me@gmail.com>
2024-02-21 13:30:52 +0100

    feat: add metadata to category page

--- src/app/categories/[slug]/[pageNumber]/page.tsx
+++ src/app/categories/[slug]/[pageNumber]/page.tsx
@@ -1,10 +1,31 @@
 import { notFound } from "next/navigation";
-import { type Route } from "next";
+import { type Route, type Metadata } from "next";
 import { executeGraphql } from "@/api/graphqlApi";
 import { ProductsGetListByCategorySlugDocument } from "@/gql/graphql";
 import { Pagination } from "@/ui/molecules/Pagination";
 import { ProductList } from "@/ui/organism/ProductList";
 import { Heading } from "@/ui/atoms/Heading";
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { slug: string };
+}): Promise<Metadata> {
+	const { category } = await executeGraphql(
+		ProductsGetListByCategorySlugDocument,
+		{
+			slug: params.slug,
+		},
+	);
+
+	if (!category) {
+		return {};
+	}
+
+	return {
+		title: category.name,
+	};
+}
 
 export default async function SingleCategoryPage({
 	params,
