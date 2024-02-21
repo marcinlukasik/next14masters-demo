6fc1b3aad9f43d002df40867abbd2b7ec0527bb2
Marcin Lukasik <marcinlukasik.me@gmail.com>
2024-02-21 14:27:29 +0100

    feat: update collection

--- src/ui/organism/SingleCollection.tsx
+++ src/ui/organism/SingleCollection.tsx
@@ -11,9 +11,9 @@ export const SingleCollection = ({
 			<p className="text-base font-medium leading-7 text-orange-500">
 				Collection
 			</p>
-			<h2 className="mb-6 mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+			<h1 className="mb-6 mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
 				{collection.name}
-			</h2>
+			</h1>
 			<p className="mb-12 text-lg leading-8 text-gray-600">
 				{collection.description}
 			</p>
