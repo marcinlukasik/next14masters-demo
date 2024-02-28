cf50ae113604833ffe988af297238f6b92655e82
Marcin Lukasik <marcinlukasik.me@gmail.com>
2024-02-28 12:08:21 +0100

    feat: add opengraph-image.tsx

---
+++ src/app/product/[productId]/opengraph-image.tsx
@@ -0,0 +1,44 @@
+import { ImageResponse } from "next/og";
+import NextImage from "next/image";
+import { getProductById } from "@/api/products";
+
+export const runtime = "edge";
+
+export const alt = "Product image for social sharing";
+export const size = {
+	width: 1200,
+	height: 630,
+};
+export const contentType = "image/png";
+
+export default async function Image({
+	params,
+}: {
+	params: { productId: string };
+}) {
+	const product = await getProductById(params.productId);
+
+	if (!product) {
+		return null;
+	}
+
+	return new ImageResponse(
+		(
+			<div tw="font-sans w-full text-white h-full flex flex-col justify-center items-center bg-orange-600 p-5">
+				{product.images[0] && (
+					<img
+						src={product.images[0].url}
+						alt={product.images[0].alt}
+						width="150"
+						height="150"
+					/>
+				)}
+				<p tw="uppercase m-6 text-5xl leading-4">{product.name}</p>
+				{product.categories[0] && (
+					<p tw="mt-4 text-2xl">{product.categories[0].name}</p>
+				)}
+				<p tw="mt-2 text-lg text-center">{product.description}</p>
+			</div>
+		),
+	);
+}
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -23,10 +23,6 @@ export async function generateMetadata({
 			title: product.name,
 			description: product.description,
 			type: "website",
-			images: product.images.map((image) => ({
-				url: image.url,
-				alt: image.alt,
-			})),
 		},
 	};
 }
